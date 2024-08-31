d92f21c4769e4bd1fe49727b378c790223f99127
Hilda Enyioko <enyiokohilda@gmail.com>
2024-08-31 15:02:30 +0100

    desktop view complete

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 // Toggle Dropdown Menu
 const navLinks = document.querySelectorAll(".nav-link");
+
 const dropdowns = document.querySelectorAll(".dropdown");
 
 navLinks.forEach((navLink, index) => {
+
     const dropdown = dropdowns[index];
 
     navLink.addEventListener('mouseenter', 
         () => {
         dropdowns.forEach(d => d.classList.remove('active'));
+
         dropdown.classList.add('active');
     });
 
@@ -40,32 +43,91 @@ navLinks.forEach((navLink, index) => {
 
 // Slide Section
 const slides = document.querySelectorAll(".slide");
+
 const slideLength = slides.length;
+
 let currentSlide = 0;
+
 const slideButtonLeft = document.querySelector('#slide-button-left');
+
 const slideButtonRight = document.querySelector('#slide-button-right');
+
 const slideCounters = document.querySelectorAll('.slide-counter');
 
 function changeSlideForward() {
     slides[currentSlide].classList.remove('slide-active');
+
     slideCounters[currentSlide].classList.remove('active');
+
     currentSlide = (currentSlide + 1) % slideLength;
+
     slides[currentSlide].classList.add('slide-active');
+
     slideCounters[currentSlide].classList.add('active');
 }
 
 function changeSlideBackward() {
+
     slides[currentSlide].classList.remove('slide-active');
+
     slideCounters[currentSlide].classList.remove('active');
+
     currentSlide = (currentSlide - 1 + slideLength) % slideLength;
+
     slides[currentSlide].classList.add('slide-active');
+
     slideCounters[currentSlide].classList.add('active');
+
 }
 
 setInterval(changeSlideForward, 3000);
 
 slideButtonLeft.addEventListener('click', changeSlideBackward);
+
 slideButtonRight.addEventListener('click', changeSlideForward);
+
+
+
+// Editor Section
+const firstStyle = document.querySelector('.pick-style1');
+
+const secondStyle = document.querySelector('.pick-style2');
+
+const firstPick = document.querySelector('.pick-image1');
+
+const secondPick = document.querySelector('.pick-image2');
+
+
+function setDimensions (baseElement, targetElement, widthPercentage, heightPercentage) {
+
+    if (baseElement && targetElement) {
+
+        const baseWidth = baseElement.offsetWidth;
+
+        const baseHeight = baseElement.offsetHeight;
+        
+        const targetWidth = baseWidth * (widthPercentage / 100);
+
+        const targetHeight = baseHeight * (heightPercentage / 100);
+        
+        targetElement.style.width = targetWidth + 'px';
+
+        targetElement.style.height = targetHeight + 'px';
+    }
+
+    else {
+        console.error('Base or target element is missing');
+    }
+
+}
+
+setDimensions(firstPick, firstStyle, 80, 80);
+
+setDimensions(secondPick, secondStyle, 80, 80);
+
+
+
+
 
 
 // Mini-Slide
